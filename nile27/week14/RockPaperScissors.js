05816917f4f89fcd1c71f894151f685b6337e5a1
nile27 <ddj03104@gmail.com>
2023-04-06 20:38:04 +0900

    feat(CPT) : 멱집합

---
+++ nile27/week14/RockPaperScissors.js
@@ -0,0 +1,26 @@
+/**
+ * 총 사람의 수인 n을 받아 가위바위보의 총 경우의 수를 구하시오 
+ * 단 n이 없는 경우 3으로 초기화 시킨다.
+ */
+
+function rockPaperScissors (n = 3) {
+
+  let arr = ["rock", "paper", "scissors"]
+  
+  let result = [];
+
+  function dfs(count, arr, stack) {
+    if (count === 0) {
+      result.push(stack)
+      return
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      let pick = arr[i]
+      dfs(count - 1, arr, stack.concat(pick))
+    }
+  }
+
+  dfs(n, arr, [])
+  return result
+};
